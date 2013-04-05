65033141fead29fa7d4f45986bad30309a493ba1
Julian <julian.mayer@craze.de>
2013-04-05 13:24:59 +0200

    first version of Figure incl. JUnit test

---
+++ test/models/FigureTest.java
@@ -0,0 +1,58 @@
+package models;
+
+import static org.junit.Assert.*;
+
+import java.awt.Point;
+
+import javax.swing.text.Position;
+
+import org.junit.Before;
+import org.junit.Test;
+
+public class FigureTest {
+	
+	Cell startPosition = new Cell();
+
+	@Test
+	public void createInstance() {
+		Figure figure = getBlackFigure();
+		
+		assertNotNull(figure);
+		assertTrue(figure.isAlive());
+		assertFalse(figure.isCrowned());
+		assertNotNull(figure.getPossibleMoves());
+		assertNotNull(figure.getPosition());
+		assertEquals(figure.getColor(), Figure.COLOR.black);
+	}
+
+	@Test
+	public void killFigure() {
+		Figure figure = getBlackFigure();
+		figure.setAlive(false);
+		
+		assertFalse(figure.isAlive());
+	}
+	
+	@Test
+	public void crownFigure() {
+		Figure figure = getBlackFigure();
+		figure.setCrowned(true);
+		
+		assertTrue(figure.isCrowned());
+	}
+	
+	@Test
+	public void changePosition() {
+		Figure figure = getBlackFigure();
+		
+		Cell newPosition = new Cell();
+		figure.setPosition(newPosition);
+		
+		assertEquals(figure.getPosition(), newPosition);
+		assertNotSame(startPosition, newPosition);
+	}
+	
+	private Figure getBlackFigure() {
+		return new Figure(startPosition, Figure.COLOR.black);
+	}
+}
