1f68461dc0473e7e5e69ffd7466c0842730ab270
maloeven <maloeven.htwg-konstanz.de>
2013-04-05 13:23:48 +0200

    commited tests, field and figure

---
+++ test/models/FieldTest.java
@@ -0,0 +1,15 @@
+package models;
+
+import static org.junit.Assert.*;
+
+import org.junit.Test;
+
+public class FieldTest {
+
+	@Test
+	public void createInstance() {
+		Field field = new Field();
+		assertNotNull(field);
+	}
+
+}
