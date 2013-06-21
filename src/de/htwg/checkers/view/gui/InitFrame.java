69722b3e90adf888b5759f5f67061db25f5ea4a2
maloeven <maloeven.htwg-konstanz.de>
2013-06-21 13:34:49 +0200

    added exit and getsize

--- src/de/htwg/checkers/view/gui/InitFrame.java
+++ src/de/htwg/checkers/view/gui/InitFrame.java
@@ -3,19 +3,28 @@ package de.htwg.checkers.view.gui;
 import java.awt.*;
 import java.awt.event.ActionEvent;
 import java.awt.event.ActionListener;
+import java.util.Observable;
+
 import javax.swing.*;
 
+import de.htwg.checkers.Checkers;
 
-public class InitFrame implements ActionListener {
+
+public class InitFrame extends Observable implements ActionListener {
 	
 	private JFrame initFrame;
 	private JPanel panel;
 	private JButton startButton;
+	private JRadioButton radioButton8x8;
+	private JRadioButton radioButton10x10;
+	private JRadioButton radioButton12x12;
 	private JLabel label1;
 	private JLabel label2;
-	private JTextField gameSizeTextField;
+	private int size;
+	//private JTextField gameSizeTextField;
 	
 	public InitFrame(){
+
 		initFrame = new JFrame("Checkers");
 		initFrame.setDefaultCloseOperation(JFrame.DISPOSE_ON_CLOSE);
 		initFrame.setLocationRelativeTo(null);
@@ -23,20 +32,30 @@ public class InitFrame implements ActionListener {
 	
 		panel = new JPanel();
 		panel.setBorder(BorderFactory.createLineBorder(Color.black));
-
 		
 		
 		startButton = new JButton("Start");
 		label1 = new JLabel("Welcome to checkers!");
-		label2 = new JLabel("Please enter size of the Gamefield, min 4.");
-		gameSizeTextField = new JTextField("8",15);	
+		label2 = new JLabel("Please choose the size of the gamefield:");
+		radioButton8x8 = new JRadioButton("8x8");
+		radioButton10x10 = new JRadioButton("10x10");
+		radioButton12x12 = new JRadioButton("12x12");
+		//gameSizeTextField = new JTextField("8",15);	
 		
 		startButton.addActionListener(this); 
 		
 		panel.add(label1);
 		panel.add(label2);
-		panel.add(gameSizeTextField);
+		//panel.add(gameSizeTextField);
+		panel.add(radioButton8x8);
+		panel.add(radioButton10x10);
+		panel.add(radioButton12x12);
 		panel.add(startButton);
+		
+		ButtonGroup group = new ButtonGroup();
+	    group.add(radioButton8x8);
+	    group.add(radioButton10x10);
+	    group.add(radioButton12x12);
 		
 		initFrame.getContentPane().add(panel);
 				
@@ -45,8 +64,18 @@ public class InitFrame implements ActionListener {
 
 	@Override
 	public void actionPerformed(ActionEvent e) {
-		int fieldSize =  Integer.parseInt(gameSizeTextField.getText());	
-		GameFrame gameFrame = new GameFrame(fieldSize);
+		//int fieldSize =  Integer.parseInt(gameSizeTextField.getText());	
+		//GameFrame gameFrame = new GameFrame(fieldSize);
+		
+		notify();
+		
 	}
 	
+	public int getSize() {
+		return size;
+	}
+	
+	public void exit(){
+		initFrame.dispose();
+	}
 }
